078940957eceb9fd896e4864bb54972f60f5d424
Phong Chau <phongchau.120498@gmail.com>
2026-05-26 00:28:11 +0700

    app

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
-import LandingPage from './components/pages/LandingPage';
 
 export default function Root() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/app" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/app" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
   );
